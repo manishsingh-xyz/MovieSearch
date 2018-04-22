3d6787d05e573df7c0b50aa6cdbb7edbf1a718a6
manishsingh-xyz <manish8081@gmail.com>
2018-04-23 00:15:24 +0530

    Service Worker Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
+import { ItunesdataService } from './itunesdata.service';
+import { HttpClientModule }    from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './/app-routing.module';
+import { MoviedetailComponent } from './moviedetail/moviedetail.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviedetailComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDialogModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : [],
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [ItunesdataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
